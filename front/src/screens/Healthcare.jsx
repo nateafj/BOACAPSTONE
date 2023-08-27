9b43f1f62aca05c98180dd2d17218fe65e2e18e3
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-08-27 15:27:15 -0400

    removed map from healthpage

--- front/src/screens/Healthcare.jsx
+++ front/src/screens/Healthcare.jsx
@@ -103,18 +103,7 @@ function HealthCare() {
       <Filters setSelectedFilters={setSelectedFilters} />
       <ProviderList providers={filteredProviders} />
 
-      <div class="embed-container">
-        <iframe
-          width="500"
-          height="400"
-          frameborder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-          title="Capstone Philanthropy Map"
-          src="//www.arcgis.com/apps/Embed/index.html?webmap=17fb17501cfe46fa8359e7e9d6184422&extent=-138.6174,15.1492,-51.8254,55.673&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legend=true&active_panel=details&basemap_gallery=true&disable_scroll=true&theme=dark"
-        ></iframe>
-      </div>
+     
     </div>
   );
 }
