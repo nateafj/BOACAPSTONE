46244c255d79428083be55427a5b7c79f20a5d8e
ssjulianr <129310861+ssjulianr@users.noreply.github.com>
2023-08-28 16:31:44 -0400

    Update Finance.jsx

--- front/src/screens/Finance.jsx
+++ front/src/screens/Finance.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BackgroundSlider from '../components/BackgroundSlider';
 import BudgetTracker from '../components/BdgtTrckr/BudgetTracker';
 import Header from '../components/Header';
 import '../styles/Finance.css'
@@ -7,6 +8,7 @@ const Finance = () =>{
     return(
         <>
         <Header/>
+        <BackgroundSlider/>
             <h1>Financial Literacy</h1>
             <BudgetTracker/>
         </>
